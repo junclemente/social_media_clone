e9bb3a2ad610d8701006f42629694da4a0d85175
Jun C <kjstudios858@gmail.com>
2018-11-19 18:49:15 -0800

    refactor Update setupscript to install libffi-dev and misaka

--- setupscript.sh
+++ setupscript.sh
@@ -2,7 +2,8 @@ apt-get update
 apt-get install -y python3
 apt-get install -y python3-pip
 apt-get install -y git
+apt-get install -y libffi-dev
 pip3 install django==1.11
 pip3 install django-bootstrap3
-
+pip3 install misaka
 
