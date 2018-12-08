feb2c5f929b8328543338363f92d73c3f267e61a
Jun C <kjstudios858@gmail.com>
2018-12-07 22:55:05 -0800

    refactor Create views

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .import models
 # Register your models here.
+
+
+class GroupMemberInline(admin.TabularInLine):
+    mode = models.GroupMember
+
+
+admin.site.register(models.Group)
